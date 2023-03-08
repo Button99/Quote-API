32659261c78444f07380951ca31a07ba0ffb4630
Christos Koumpis <56029580+Button99@users.noreply.github.com>
2023-03-08 20:14:01 +0200

    spit the url

--- QuoteAPI/src/index.php
+++ QuoteAPI/src/index.php
@@ -5,12 +5,16 @@ require '../vendor/autoload.php';
 use Src\app\MySQLConnection;
 
 set_exception_handler('\Src\handlers\ErrorHandler::handleException');
+set_exception_handler('\Src\handlers\ErrorHandler::handleError');
+
 
 header("Content-type: application/json; charset=UTF-8");
 
-$db= new MySQLConnection('localhost', 'Quote', 'root', '1234');
+$url= explode('/', $_SERVER['REQUEST_URI']);
+$id= $url[3] ?? null;
+
+$db= new MySQLConnection('localhost', 'QuoteDB', 'root', '');
 
 $gateway= new \Src\app\QuoteGateway($db);
 $controller= new \Src\controllers\QuoteController($gateway);
-$id= '1';
-$controller->processRequest($_SERVER['REQUEST_METHOD'], null);
+$controller->processRequest($_SERVER['REQUEST_METHOD'], $id);
