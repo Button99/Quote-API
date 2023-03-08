5ce6a293267beddfd72f7ff43685e5703a53f473
Christos Koumpis <56029580+Button99@users.noreply.github.com>
2023-03-08 19:04:41 +0200

    Added get and post

--- QuoteAPI/src/controllers/QuoteController.php
+++ QuoteAPI/src/controllers/QuoteController.php
@@ -2,15 +2,22 @@
 
 namespace Src\controllers;
 
+use Src\app\QuoteGateway;
+use Src\models\Quote;
+
 class QuoteController
 {
     // CRUD mechanism for the quotes
 
+    public function __construct(private QuoteGateway $quoteGateway) {
+
+    }
+
     public function processRequest(string $method, ?string $id): void {
         if($id) {
-
+            $this->processResourceRequest($method, $id);
         } else {
-
+            $this->processCollectionRequest($method);
         }
     }
 
@@ -21,7 +28,7 @@ class QuoteController
     private function processCollectionRequest(string $method): void {
         switch ($method) {
             case 'GET':
-                print 'GET';
+                echo json_encode($this->quoteGateway->getAll());
                 break;
             case 'POST':
                 print 'POST';
