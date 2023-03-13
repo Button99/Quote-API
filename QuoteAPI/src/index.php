166663563edf9ee96b5b81c046763c3b2d07a211
Button99 <56029580+Button99@users.noreply.github.com>
2023-03-13 15:44:50 +0200

    Changed MySQL namespace

--- QuoteAPI/src/index.php
+++ QuoteAPI/src/index.php
@@ -3,10 +3,8 @@ declare(strict_types=1);
 
 require '../vendor/autoload.php';
 
-use src\database\MySQLConnection;
-
 set_exception_handler('\Src\handlers\ErrorHandler::handleException');
-set_exception_handler('\Src\handlers\ErrorHandler::handleError');
+//set_exception_handler('\Src\handlers\ErrorHandler::handleError');
 
 
 header("Content-type: application/json; charset=UTF-8");
@@ -14,7 +12,7 @@ header("Content-type: application/json; charset=UTF-8");
 $url= explode('/', $_SERVER['REQUEST_URI']);
 $id= $url[3] ?? null;
 
-$db= new MySQLConnection('localhost', 'QuoteDB', 'root', '');
+$db= new \Src\Database\MySQLConnection('localhost', 'QuoteDB', 'root', '');
 
 $gateway= new \Src\app\QuoteGateway($db);
 $controller= new \Src\controllers\QuoteController($gateway);
